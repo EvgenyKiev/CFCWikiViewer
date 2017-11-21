21374fc6b2708cb25e8d370531db09cf2b000a33
Evgenykiev <padakluev@gmx.com>
2017-11-21 19:31:49 +0200

    change

--- js/js_searcher.js
+++ js/js_searcher.js
@@ -21,30 +21,77 @@ window.addEventListener("load", function(event) {
 
                     var jpars=JSON.parse(xhr.responseText)
 
-                    var title= jpars[1][0]
-                    var snipp=jpars[2][0]
-                    var link=jpars[3][0]
+                    // var title= jpars[1][0]
+                    // var snipp=jpars[2][0]
+                    // var link=jpars[3][0]
 
-                    console.log (jpars.length)
-                    console.log (jpars[1].length)
-                    console.log (jpars[2].length)
-                    console.log (jpars[3].length)
+                    // console.log (jpars.length)
+                    // console.log (jpars[1].length)
+                    // console.log (jpars[2].length)
+                    // console.log (jpars[3].length)
+
+                    //  cycle
+
+                    for(  var i=0;i<jpars[1].length;i++) {
+                        var title= jpars[1][i];
+                        var snipp=jpars[2][i];
+                        var link=jpars[3][i];
+
+
+                        //console.log('qwrtyui')
+
+                        //test titel
+                        //console.log(title)
+
+                        //snipp
+                         //console.log (snipp)
+
+                        //
+                        // link
+                       //console.log (link)
+
+                    }
+
+
+                        //end   cycle
 
 
 
-
-
-                    console.log (title +' ' + snipp +' '+link)
-
-                    ///
+                    ///insert
 
                     var paras = document.getElementsByClassName("row");
-                    var innahtml='<div class="row" ><div class="col-sm" ></div><div class="col-sm-6"><h2>'+title+'</h2><p><a class="snippet" href ='+link+'>'+snipp+'</a></p></div><div class="col-sm"></div></div>'
+
                     var d1 = document.getElementById('insert');
 
+
+
+                    // function insertt () {
+                    //     d1.insertAdjacentHTML('beforeend',innahtml );
+                    // }
+
+
+                    //  cycle
+
                     function insertt () {
-                        d1.insertAdjacentHTML('beforeend',innahtml );
+
+                        for (var i = 0; i < jpars[1].length; i++) {
+                            var title = jpars[1][i];
+                            var snipp = jpars[2][i];
+                            var link = jpars[3][i];
+
+                            var innahtml = '<div class="row" ><div class="col-sm" ></div><div class="col-sm-6"><h2>' + title + '</h2><p><a class="snippet" href =' + link + '>' + snipp + '</a></p></div><div class="col-sm"></div></div>'
+                            d1.insertAdjacentHTML('beforeend', innahtml)
+
+                        }
                     }
+
+
+                    //end   cycle
+
+
+
+
+
 
 
                     if (paras) {
@@ -52,6 +99,8 @@ window.addEventListener("load", function(event) {
                         Array.prototype.forEach.call(paras, function (para) {
                             if (para.getAttribute("class") === "row")
                                 para.parentNode.removeChild(para);
+
+
                         });
 
                         insertt ();
@@ -61,6 +110,16 @@ window.addEventListener("load", function(event) {
                     else {
                         insertt ()
                     }
+
+
+                    /// end insert
+
+
+
+
+
+                   // console.log (title +' ' + snipp +' '+link)
+
 
 
 
