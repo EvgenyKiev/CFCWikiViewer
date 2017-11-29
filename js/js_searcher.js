18feb3b1f0aaaf9a2e163273dbf7b7ca892912a5
Evgenykiev <padakluev@gmx.com>
2017-11-29 20:12:38 +0200

    change

--- js/js_searcher.js
+++ js/js_searcher.js
@@ -8,7 +8,7 @@ window.addEventListener("load", function(event) {
         console.log("Look up: ",aaa);
         //console.log(wiki)
 
-        var wiki='https://en.wikipedia.org/w/api.php?action=opensearch&search=%'+aaa+'&prop=info&format=json&origin=*&inprop=url'
+        var wiki='https://en.wikipedia.org/w/api.php?action=opensearch&search=%'+aaa+'&prop=info&format=json&origin=*&inprop=url';
 
 
 
@@ -21,39 +21,15 @@ window.addEventListener("load", function(event) {
 
                     var jpars=JSON.parse(xhr.responseText)
 
-                    // var title= jpars[1][0]
-                    // var snipp=jpars[2][0]
-                    // var link=jpars[3][0]
-
-                    // console.log (jpars.length)
-                    // console.log (jpars[1].length)
-                    // console.log (jpars[2].length)
-                    // console.log (jpars[3].length)
-
-                    //  cycle
 
                     for(  var i=0;i<jpars[1].length;i++) {
-                        var title= jpars[1][i];
-                        var snipp=jpars[2][i];
-                        var link=jpars[3][i];
+                        // var title= jpars[1][i];
+                        // var snipp=jpars[2][i];
+                        // var link=jpars[3][i];
 
-
-                        //console.log('qwrtyui')
-
-                        //test titel
-                        //console.log(title)
-
-                        //snipp
-                         //console.log (snipp)
-
-                        //
-                        // link
-                       //console.log (link)
 
                     }
 
-
-                        //end   cycle
 
 
 
@@ -67,7 +43,9 @@ window.addEventListener("load", function(event) {
                         for (var i = 0; i < jpars[1].length; i++) {
 
                             Array.prototype.forEach.call(paras, function (para) {
+
                                 if (para.getAttribute("class") === "row")
+
                                     para.parentNode.removeChild(para);
 
                                 console.log('if (paras) delete')
@@ -80,7 +58,7 @@ window.addEventListener("load", function(event) {
                     }
 
                     insertt ();
-                    console.log ('true insert')
+                    // console.log ('true insert')
 
 
                     /// end insert
@@ -92,7 +70,7 @@ window.addEventListener("load", function(event) {
                             var snipp = jpars[2][i];
                             var link = jpars[3][i];
 
-                            var innahtml = '<div class="row" ><div class="col-sm" ></div><div class="col-sm-6"><h2>' + title + '</h2><p><a class="snippet" href =' + link + '>' + snipp + '</a></p></div><div class="col-sm"></div></div>'
+                            var innahtml = '<div class="row" ><div class="col-md-4" ></div><div class="col-sm-6"><h2>' + title + '</h2><p><a class="snippet" href =' + link + '>' + snipp + '</a></p></div><div class="col-md-4"></div></div>';
                             d1.insertAdjacentHTML('beforeend', innahtml)
 
                         }
